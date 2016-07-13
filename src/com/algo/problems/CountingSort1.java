31cf172d513b1b59063650044659a13b27c5c6b4
baddie <baddie@192.168.0.4>
2016-07-13 07:20:21 +0530

    Counting Sort Problems

---
+++ src/com/algo/problems/CountingSort1.java
@@ -0,0 +1,24 @@
+package com.algo.problems;
+
+import java.util.Scanner;
+
+public class CountingSort1 {
+
+	public static void main(String[] args) {
+		try (Scanner in = new Scanner(System.in)) {
+			int T = in.nextInt();
+			int[] arr = new int[100];
+			for (int i = 0; i < T; i++) {
+				int num = in.nextInt();
+				arr[num]+=1; 
+			}
+			for (int i :arr) {
+				System.out.print(i+" ");
+			}
+			System.out.println();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+}
---
+++ src/com/algo/problems/CountingSort2.java
@@ -0,0 +1,25 @@
+package com.algo.problems;
+
+import java.util.Scanner;
+
+public class CountingSort2 {
+
+	public static void main(String[] args) {
+		try (Scanner in = new Scanner(System.in)) {
+			int T = in.nextInt();
+			int[] arr = new int[100];
+			for (int i = 0; i < T; i++) {
+				int num = in.nextInt();
+				arr[num]+=1; 
+			}
+			for (int i=0;i<100;i++) {
+				for(int j=arr[i];j>0;j--)
+				System.out.print(i+" ");
+			}
+			System.out.println();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+}
